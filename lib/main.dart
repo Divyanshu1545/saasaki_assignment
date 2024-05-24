90b70d20db7653d30f0d172182ca716ef2c41be8
divyanshu1545 <daroliadivyanshu45@gmail.com>
2024-05-24 15:47:19 +0530

    OTP is working properly

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:saasaki_assignment/firebase_options.dart';
 import 'package:saasaki_assignment/providers/authentication_provider.dart';
 import 'package:saasaki_assignment/screens/home_screen.dart';
 import 'package:saasaki_assignment/screens/otp_screen.dart';
@@ -10,7 +12,8 @@ import 'package:saasaki_assignment/screens/splash_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseAppCheck.instance.activate();
   runApp(MyApp());
 }
 
